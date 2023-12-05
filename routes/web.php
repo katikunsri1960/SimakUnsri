a07fedcf14993778ec4bafe7888f79ea0ca82ae2
UPT TIK Unsri <ka.tik@unsri.ac.id>
2023-12-05 16:41:51 +0700

    Add authentication middleware to home route

--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,9 @@ Auth::routes([
     'register' => false
 ]);
 
+Route::group(['middleware' => ['auth']], function() {
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    
+});
+
