973a8b92e0f78dd52f2ef8899829706b8580c62c
UPT TIK Unsri <ka.tik@unsri.ac.id>
2023-12-05 17:02:02 +0700

    Remove unnecessary route definition

--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,6 @@ Auth::routes([
 ]);
 
 Route::group(['middleware' => ['auth']], function() {
-
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-    
 });
 
