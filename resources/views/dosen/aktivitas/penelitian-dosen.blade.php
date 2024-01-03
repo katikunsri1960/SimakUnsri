1d380ccaefdc6280894664a66e671aef24408970
Noordin As-Shiddiq Mangkunegara <47419175+shiddiqmangkunegara@users.noreply.github.com>
2024-01-03 13:33:20 +0700

    Pembuatan halaman aktivitas dosen (penelitian, publikasi, dan pengabdian)

---
+++ resources/views/dosen/aktivitas/penelitian-dosen.blade.php
@@ -0,0 +1,62 @@
+@extends('layouts.dosen')
+@section('title')
+Penelitian Dosen
+@endsection
+@section('content')
+<section class="content bg-white">
+    <div class="row align-items-end">
+        <div class="col-12">
+			<div class="box pull-up">
+				<div class="box-body bg-img bg-primary-light">
+					<div class="d-lg-flex align-items-center justify-content-between">
+						<div class="d-lg-flex align-items-center mb-30 mb-xl-0 w-p100">
+			    			<img src="{{asset('images/images/svg-icon/color-svg/custom-14.svg')}}" class="img-fluid max-w-250" alt="" />
+							<div class="ms-30">
+								<h2 class="mb-10">Penelitian Dosen</h2>
+								<p class="mb-0 text-fade fs-18">Universitas Sriwijaya</p>
+							</div>
+						</div>
+					<div>
+				</div>
+			</div>							
+		</div>
+    </div>
+    <div class="row">
+        <div class="col-xxl-12">
+            <div class="box box-body mb-0 bg-light">
+                <div class="row">
+                    <div class="col-xl-12 col-lg-12">
+                        <h3 class="fw-500 text-dark mt-0">Daftar Penelitian Dosen</h3>
+                    </div>                             
+                </div>
+                <div class="row">
+                    <div class="table-responsive">
+                        <table id="example1" class="table table-bordered table-striped text-center">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>NIP</th>
+                                    <th>Nama Dosen</th>
+                                    <th>Nama Ketua Penelitian</th>
+                                    <th>Tahun Penelitian</th>
+                                    <th>Publisher</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Customer Support</td>
+                                    <td>New York</td>
+                                    <td>New York</td>
+                                    <td>New York</td>
+                                </tr>
+                            </tbody>
+					  </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>			
+</section>
+@endsection
+
