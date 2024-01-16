42a8e79d6a62ee17781eaffa1575db0c5a544378
UPT TIK Unsri <ka.tik@unsri.ac.id>
2024-01-16 23:44:21 +0700

    Add foreign key to UserSeeder

---
+++ database/migrations/2024_01_16_163915_create_jenis_keluars_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jenis_keluars', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_jenis_keluar')->unique();
+            $table->string('jenis_keluar');
+            $table->boolean('apa_mahasiswa');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jenis_keluars');
+    }
+};
