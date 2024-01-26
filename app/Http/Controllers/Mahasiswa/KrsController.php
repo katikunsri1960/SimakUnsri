60760f7714e1db2aac2b49f59462086167a512be
Ryan Fitra Perdana <36030822+ryanfitra@users.noreply.github.com>
2024-01-26 13:01:23 +0700

    mulai krs mahasiswa

--- app/Http/Controllers/Mahasiswa/KrsController.php
+++ app/Http/Controllers/Mahasiswa/KrsController.php
@@ -2,13 +2,27 @@
 
 namespace App\Http\Controllers\Mahasiswa;
 
-use App\Http\Controllers\Controller;
+use App\Models\Wilayah;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Mahasiswa\BiodataMahasiswa;
+use App\Models\Mahasiswa\RiwayatPendidikan;
+use App\Models\Perkuliahan\MatkulKurikulum;
+use App\Models\Semester;
 
 class KrsController extends Controller
 {
-    public function index()
+    public function krs()
     {
-        return view('mahasiswa.krs');
+
+        $id_reg = auth()->user()->fk_id;
+        $semester_aktif = Semester::where('id_semester', 20231)->get();
+
+        $daftar_mk = MatkulKurikulum::where('semester', $semester_aktif->id_semester)->first();
+        // dd($semester_aktif);
+        
+
+                
+        return view('mahasiswa.krs.index', compact('semester_aktif'));
     }
 }
