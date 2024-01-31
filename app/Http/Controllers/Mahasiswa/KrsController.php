2b8f23ea850a011db4b81854e1bd53fa51d78646
Ryan Fitra Perdana <36030822+ryanfitra@users.noreply.github.com>
2024-01-31 13:46:52 +0700

    update krs

--- app/Http/Controllers/Mahasiswa/KrsController.php
+++ app/Http/Controllers/Mahasiswa/KrsController.php
@@ -22,43 +22,28 @@ class KrsController extends Controller
 
         $id_reg = auth()->user()->fk_id;
 
-        $id_prodi = RiwayatPendidikan::where('id_registrasi_mahasiswa', $id_reg)
-            ->pluck('id_prodi')->first();
+        $riwayat_pendidikan = RiwayatPendidikan::where('id_registrasi_mahasiswa', $id_reg)
+                            ->first();
 
         $semester_aktif = Semester::where('id_semester', '20231')->first();
-        // dd($semester_aktif);
-
+        
         $semester_ke = AktivitasKuliahMahasiswa::where('id_registrasi_mahasiswa', $id_reg)->count();
-            
-        $semester_masuk = RiwayatPendidikan::where('id_registrasi_mahasiswa', $id_reg)->pluck('id_periode_masuk')->first();
-        // dd($semester_ke);
 
         $matakuliah =  MataKuliah::leftJoin('kelas_kuliahs', 'mata_kuliahs.id_matkul', '=', 'kelas_kuliahs.id_matkul')
-            ->select('mata_kuliahs.id_matkul','mata_kuliahs.kode_mata_kuliah', 'mata_kuliahs.nama_mata_kuliah', 'mata_kuliahs.sks_mata_kuliah')
-            ->where('mata_kuliahs.id_prodi', $id_prodi)
-            // ->where('matkul_kurikulums.semester', '<=',$semester_ke)
-            ->where('kelas_kuliahs.id_semester',  $semester_aktif->id_semester) 
-            // ->orderBy('semester')
-            ->groupBy('mata_kuliahs.id_matkul','mata_kuliahs.kode_mata_kuliah', 'mata_kuliahs.nama_mata_kuliah', 'mata_kuliahs.sks_mata_kuliah')
-            ->orderBy('sks_mata_kuliah')
-            // ->limit(10)
-            ->get();
-
-
-            // dd($matakuliah);
+                    ->select('mata_kuliahs.id_matkul','mata_kuliahs.kode_mata_kuliah', 'mata_kuliahs.nama_mata_kuliah', 'mata_kuliahs.sks_mata_kuliah')
+                    ->where('mata_kuliahs.id_prodi', $riwayat_pendidikan->id_prodi)
+                    ->where('kelas_kuliahs.id_semester',  $semester_aktif->id_semester) 
+                    ->groupBy('mata_kuliahs.id_matkul','mata_kuliahs.kode_mata_kuliah', 'mata_kuliahs.nama_mata_kuliah', 'mata_kuliahs.sks_mata_kuliah')
+                    ->orderBy('sks_mata_kuliah')
+                    ->get();
 
        $kelas_kuliah = KelasKuliah::with(['dosen_pengajar'])
-            ->where('id_prodi', $id_prodi)
-            ->where('id_semester',  $semester_aktif->id_semester) 
-            ->select('id_matkul', 'id_kelas_kuliah', 'nama_mata_kuliah', 'nama_kelas_kuliah', 'tanggal_mulai_efektif', 'id_semester' )
-            ->groupBy('id_matkul', 'id_kelas_kuliah', 'nama_mata_kuliah', 'nama_kelas_kuliah', 'tanggal_mulai_efektif', 'id_semester' )
-            ->orderBy('nama_kelas_kuliah', 'ASC')
-            // ->where('nama_mata_kuliah', 'PROPOSAL SKRIPSI')
-            // ->where('id_semester', $matakuliah_kurikulum[1]->id_semester)
-            // ->limit(10)
-            ->get();
-            // dd($kelas_kuliah);
-
+                    ->where('id_prodi', $riwayat_pendidikan->id_prodi)
+                    ->where('id_semester',  $semester_aktif->id_semester) 
+                    ->select('id_matkul', 'id_kelas_kuliah', 'nama_mata_kuliah', 'nama_kelas_kuliah', 'tanggal_mulai_efektif', 'id_semester' )
+                    ->groupBy('id_matkul', 'id_kelas_kuliah', 'nama_mata_kuliah', 'nama_kelas_kuliah', 'tanggal_mulai_efektif', 'id_semester' )
+                    ->orderBy('nama_kelas_kuliah', 'ASC')
+                    ->get();
 
         return view('mahasiswa.krs.index', compact(
             'matakuliah', 
--- resources/views/mahasiswa/krs/include/data-kelas-kuliah.blade.php
+++ resources/views/mahasiswa/krs/include/data-kelas-kuliah.blade.php
@@ -26,7 +26,6 @@
                                                             <th class="text-center align-middle">No</th>
                                                             <th class="text-center align-middle">Kode Mata Kuliah</th>
                                                             <th class="text-center align-middle">Nama Mata Kuliah</th>                                    
-                                                            {{-- <th class="text-center align-middle">Semester</th> --}}
                                                             <th class="text-center align-middle">SKS Mata Kuliah</th>
                                                             <th class="text-center align-middle">Jadwal Kuliah</th>
                                                             <th class="text-center align-middle">Action</th>
@@ -38,85 +37,60 @@
                                                         @endphp
 
                                                         @foreach ($matakuliah as $data)
-                                                            {{-- @foreach ($data->kelas_kuliah as $kelas) --}}
-                                                                {{-- @if($kelas->id_semester == $semester_aktif->id_semester) --}}
-                                                                
-                                                            
-                                                                <tr>
-                                                                    <td class="text-center align-middle">{{ $no_a++ }}</td>
-                                                                    <td class="text-center align-middle">{{$data->kode_mata_kuliah}}</td>
-                                                                    <td class="text-start align-middle">{{$data->nama_mata_kuliah}}</td>
-                                                                    <td class="text-center align-middle">{{$data->sks_mata_kuliah}}</td>
-                                                                    
-                                                                    <td>
-                                                                        {{-- <button type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample"
-                                                                                    class="waves-effect waves-light btn btn-success-light mb-5">
-                                                                                Lihat Kelas Kuliah
-                                                                        </button> --}}
-                                                                        {{-- <div class="btn-group">
-                                                                            <button class="waves-effect rounded waves-light btn btn-success-light mb-5 no-caret" type="button" data-bs-toggle="dropdown">Lihat Kelas Kuliah</button>
-                                                                            <div class="dropdown-menu">
-                                                                                <a class="dropdown-item" href="#">Action</a>
-                                                                                <a class="dropdown-item" href="#">Another action</a>
-                                                                                <a class="dropdown-item" href="#">Something else here</a>
-                                                                            </div>
+                                                            <tr>
+                                                                <td class="text-center align-middle">{{ $no_a++ }}</td>
+                                                                <td class="text-center align-middle">{{$data->kode_mata_kuliah}}</td>
+                                                                <td class="text-start align-middle">{{$data->nama_mata_kuliah}}</td>
+                                                                <td class="text-center align-middle">{{$data->sks_mata_kuliah}}</td>
+                                                                <td>
+                                                                    <p>
+                                                                        <button class="waves-effect rounded waves-light btn btn-success-light mb-5 no-caret" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample{{$data->id_matkul}}" aria-expanded="false" aria-controls="collapseExample">
+                                                                            Lihat Kelas Kuliah
+                                                                        </button>
+                                                                    </p>
+                                                                    <div class="collapse" id="collapseExample{{$data->id_matkul}}">
+                                                                        <div class="card card-body">
+                                                                            <table id="data-kelas" class="table table-bordered table-striped text-center">
+                                                                                <thead>
+                                                                                    <tr>
+                                                                                        <th class="text-center align-middle">No</th>
+                                                                                        <th class="text-center align-middle">Kelas Kuliah</th>
+                                                                                        <th class="text-center align-middle">Dosen Pengajar</th>
+                                                                                        <th class="text-center align-middle">Jadwal Kuliah</th>
+                                                                                        <th class="text-center align-middle">Peserta</th>
+                                                                                    </tr>
+                                                                                </thead>
+                                                                                <tbody>
+                                                                                    @php
+                                                                                        $no=1;
+                                                                                    @endphp
+                            
+                                                                                    @foreach ($kelas_kuliah as $d)
+                                                                                        @if($d->id_matkul == $data->id_matkul)
+                                                                                            <tr>
+                                                                                                <td class="text-center align-middle">{{ $no++ }}</td>
+                                                                                                <td class="text-center align-middle">{{$d->nama_kelas_kuliah}}</td>
+                                                                                                <td class="text-start align-middle w-300 d-xl-inline-block">
+                                                                                                    @foreach ($d->dosen_pengajar as $dosen)
+                                                                                                        <i class="fa-regular fa-circle-dot"></i> {{$dosen->nama_dosen}}<br>
+                                                                                                    @endforeach
+                                                                                                </td>
+                                                                                                <td class="text-start align-middle">{{$d->tanggal_mulai_efektif}}</td>
+                                                                                                <td class="text-center align-middle">-</td>
+                                                                                                <td class="text-center align-middle">
+                                                                                                    <input name="group5" type="radio" id="radio_{{$d->id_kelas_kuliah}}" class="with-gap radio-col-success" />
+                                                                                                    <label for="radio_{{$d->id_kelas_kuliah}}"></label>
+                                                                                                </td>
+                                                                                            </tr>
+                                                                                        @endif
+                                                                                    @endforeach
+                                                                                </tbody>
+                                                                            </table>
                                                                         </div>
-                                                                        <div class="collapse" id="collapseExample">
-                                                                            <div class="card card-body">
-                                                                                
-                                                                            </div>
-                                                                        </div> --}}
-
-                                                                        <p>
-                                                                            <button class="waves-effect rounded waves-light btn btn-success-light mb-5 no-caret" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample{{$data->id_matkul}}" aria-expanded="false" aria-controls="collapseExample">
-                                                                                Lihat Kelas Kuliah
-                                                                            </button>
-                                                                        </p>
-                                                                        <div class="collapse" id="collapseExample{{$data->id_matkul}}">
-                                                                            <div class="card card-body">
-                                                                                <table id="data-kelas" class="table table-bordered table-striped text-center">
-                                                                                    <thead>
-                                                                                        <tr>
-                                                                                            <th class="text-center align-middle">No</th>
-                                                                                            <th class="text-center align-middle">Kelas Kuliah</th>
-                                                                                            <th class="text-center align-middle">Dosen Pengajar</th>
-                                                                                            <th class="text-center align-middle">Jadwal Kuliah</th>
-                                                                                            <th class="text-center align-middle">Peserta</th>
-                                                                                        </tr>
-                                                                                    </thead>
-                                                                                    <tbody>
-                                                                                        @php
-                                                                                            $no=1;
-                                                                                        @endphp
-                                
-                                                                                        @foreach ($kelas_kuliah as $d)
-                                                                                            @if($d->id_matkul == $data->id_matkul)
-                                                                                                <tr>
-                                                                                                    <td class="text-center align-middle">{{ $no++ }}</td>
-                                                                                                    <td class="text-center align-middle">{{$d->nama_kelas_kuliah}}</td>
-                                                                                                    <td class="text-start align-middle w-300 d-xl-inline-block">
-                                                                                                        @foreach ($d->dosen_pengajar as $dosen)
-                                                                                                            <i class="fa-regular fa-circle-dot"></i> {{$dosen->nama_dosen}}<br>
-                                                                                                        @endforeach
-                                                                                                    </td>
-                                                                                                    <td class="text-start align-middle">{{$d->tanggal_mulai_efektif}}</td>
-                                                                                                    <td class="text-center align-middle">-</td>
-                                                                                                    <td class="text-center align-middle">
-                                                                                                        <input name="group5" type="radio" id="radio_{{$d->id_kelas_kuliah}}" class="with-gap radio-col-success" />
-                                                                                                        <label for="radio_{{$d->id_kelas_kuliah}}"></label>
-                                                                                                    </td>
-                                                                                                </tr>
-                                                                                            @endif
-                                                                                        @endforeach
-                                                                                    </tbody>
-                                                                                </table>
-                                                                            </div>
-                                                                        </div>
-                                                                    </td>
-                                                                    <td><input type="checkbox" id="md_checkbox_23" class="filled-in chk-col-success" /><label for="md_checkbox_23"></label></td>
-                                                                </tr>
-                                                                {{-- @endif --}}
-                                                            {{-- @endforeach --}}
+                                                                    </div>
+                                                                </td>
+                                                                <td><input type="checkbox" id="md_checkbox_23" class="filled-in chk-col-success" /><label for="md_checkbox_23"></label></td>
+                                                            </tr>
                                                         @endforeach
                                                     </tbody>
                                                 </table>
