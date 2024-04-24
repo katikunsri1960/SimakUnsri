137bfcd3dd24496d18375b8bc31688de6760e50c
Noordin As-Shiddiq Mangkunegara <47419175+shiddiqmangkunegara@users.noreply.github.com>
2024-04-24 10:09:18 +0700

    Update nilai controller

--- app/Http/Controllers/Mahasiswa/NilaiController.php
+++ app/Http/Controllers/Mahasiswa/NilaiController.php
@@ -24,8 +24,9 @@ class NilaiController extends Controller
         $id_reg_mhs = auth()->user()->fk_id;
         $aktivitas_kuliah=AktivitasKuliahMahasiswa::where('id_registrasi_mahasiswa',$id_reg_mhs)->where('id_semester', $id_semester)->get();
         $nilai_mahasiswa = NilaiPerkuliahan::with('dosen_pengajar')->where('id_registrasi_mahasiswa', $id_reg_mhs)->where('id_semester', $id_semester)->orderBy('nama_mata_kuliah','asc')->get();
+        $transkrip_mahasiswa=TranskripMahasiswa::where('id_registrasi_mahasiswa',$id_reg_mhs)->orderBy('smt_diambil','asc')->get();
         // dd($dosen_pengajar[0]);
 
-        return view('mahasiswa.nilai-perkuliahan.include.detail-khs', ['data_nilai' => $nilai_mahasiswa, 'data_aktivitas' => $aktivitas_kuliah]);
+        return view('mahasiswa.nilai-perkuliahan.include.detail-khs', ['data_nilai' => $nilai_mahasiswa, 'data_aktivitas' => $aktivitas_kuliah, 'transkrip' => $transkrip_mahasiswa]);
     }
 }
