7eec29e2e9aae87ae5dbaaccde418ab5abd0a81d
UPT TIK Unsri <ka.tik@unsri.ac.id>
2024-05-25 15:56:08 +0700

    feat: Add relationships between Fakultas, Jurusan, and ProgramStudi models
    This commit adds the necessary relationships between the Fakultas, Jurusan, and ProgramStudi models. The Fakultas model now has a hasMany relationship with the Jurusan model and a hasMany relationship with the ProgramStudi model. The ProgramStudi model has a belongsTo relationship with the Jurusan model and a belongsTo relationship with the Fakultas model. These relationships enhance the data structure and allow for easier querying and navigation between related models.

--- app/Models/ProgramStudi.php
+++ app/Models/ProgramStudi.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Mahasiswa\RiwayatPendidikan;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,4 +10,19 @@ class ProgramStudi extends Model
 {
     use HasFactory;
     protected $guarded = [];
+
+    public function jurusan()
+    {
+        return $this->belongsTo(Jurusan::class, 'id_jurusan', 'jurusan_id');
+    }
+
+    public function mahasiswa()
+    {
+        return $this->hasMany(RiwayatPendidikan::class, 'id_prodi', 'prodi_id');
+    }
+
+    public function fakultas()
+    {
+        return $this->belongsTo(Fakultas::class, 'id_fakultas', 'id');
+    }
 }
