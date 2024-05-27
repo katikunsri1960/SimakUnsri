163841fcf5daba3048a9d5ff3a5c3e3daab6f800
UPT TIK Unsri <ka.tik@unsri.ac.id>
2024-05-27 16:26:12 +0700

    add column to aktivitas mahasiswas table

---
+++ database/migrations/2024_05_27_161218_add_column_to_aktivitas_mahasiswas_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('aktivitas_mahasiswas', function (Blueprint $table) {
+            $table->string('mk_konversi')->nullable()->after('status_sync');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('aktivitas_mahasiswas', function (Blueprint $table) {
+            $table->dropColumn('mk_konversi');
+        });
+    }
+};
