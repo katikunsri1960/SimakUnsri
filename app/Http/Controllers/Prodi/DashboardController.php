cac48bf6a7131fdb8059484147ddf76e1f7cfa9e
UPT TIK Unsri <ka.tik@unsri.ac.id>
2024-05-27 10:06:03 +0700

    feat: Add distinct status count for Mahasiswa in DashboardController
    This commit adds a TODO task to implement a distinct count of Mahasiswa status based on the 'keterangan_keluar' field in the 'riwayat_pendidikan' table. It also includes a commented out code snippet for retrieving the distinct status using the `RiwayatPendidikan` model. This change aims to enhance the functionality of the DashboardController in displaying the count of Mahasiswa based on their status.

--- app/Http/Controllers/Prodi/DashboardController.php
+++ app/Http/Controllers/Prodi/DashboardController.php
@@ -3,12 +3,20 @@
 namespace App\Http\Controllers\Prodi;
 
 use App\Http\Controllers\Controller;
+use App\Models\Mahasiswa\RiwayatPendidikan;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
+        // TODO : Jumlah mahasiswa Berdasarkan status Mahasiswa
+        // distinct('keterangan_keluar') from riwayat_pendidikan
+
+        $riwayat = RiwayatPendidikan::select('keterangan_keluar')->distinct()->get();
+        
+        // $status = RiwayatPendidikan::distinct('keterangan_keluar')->get();
+        // dd($status);
         return view('prodi.dashboard');
     }
 }
