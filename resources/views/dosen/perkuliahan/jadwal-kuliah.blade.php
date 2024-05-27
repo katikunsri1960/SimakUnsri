b1195146da291a83f44cbb0702894c2fdfd7d65a
Noordin As-Shiddiq Mangkunegara <47419175+shiddiqmangkunegara@users.noreply.github.com>
2024-05-27 11:34:48 +0700

    Pembuatan Tampilan Jadwal Mengajar Dosen

--- resources/views/dosen/perkuliahan/jadwal-kuliah.blade.php
+++ resources/views/dosen/perkuliahan/jadwal-kuliah.blade.php
@@ -12,7 +12,7 @@ Jadwal Kuliah Dosen
 						<div class="d-lg-flex align-items-center mb-30 mb-xl-0 w-p100">
 			    			<img src="{{asset('images/images/svg-icon/color-svg/custom-14.svg')}}" class="img-fluid max-w-250" alt="" />
 							<div class="ms-30">
-								<h2 class="mb-10">Jadwal Kuliah Dosen</h2>
+								<h2 class="mb-10">Jadwal Mengajar Dosen</h2>
 								<p class="mb-0 text-fade fs-18">Universitas Sriwijaya</p>
 							</div>
 						</div>
@@ -23,38 +23,38 @@ Jadwal Kuliah Dosen
     </div>
     <div class="row">
         <div class="col-xxl-12">
-            <div class="box box-body mb-0 bg-light">
+            <div class="box box-body mb-0">
                 <div class="row">
                     <div class="col-xl-4 col-lg-12">
-                        <h3 class="fw-500 text-dark mt-0">Daftar Jadwal Kuliah Dosen</h3>
+                        <h3 class="fw-500 text-dark mt-0">Daftar Jadwal Mengajar Dosen</h3>
                     </div>                             
                 </div>
                 <div class="row">
                     <div class="table-responsive">
-                        <table id="example1" class="table table-bordered table-striped text-center">
+                        <table id="data" class="table table-bordered table-striped text-center">
                             <thead>
                                 <tr>
                                     <th>No</th>
                                     <th>Kode Mata Kuliah</th>
                                     <th>Nama Mata Kuliah</th>                                    
-                                    <th>Kode Kelas</th>
                                     <th>Nama Kelas</th>
-                                    <th>Tahun Ajaran</th>
-                                    <th>Kesediaan Waktu Mengajar</th>
+                                    <th>Ruang Perkuliahan</th>
+                                    <th>Semester</th>
+                                    <th>Jadwal Kuliah</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td>1</td>
-                                    <td>Customer Support</td>
-                                    <td>New York</td>
-                                    <td>New York</td>
-                                    <td>New York</td>
-                                    <td>10</td>
-                                    <td>
-                                        <a class="btn btn-rounded bg-success-light" href="{{route('dosen.perkuliahan.kesediaan-waktu-kuliah')}}"><i class="fa fa-calendar-plus-o"><span class="path1"></span><span class="path2"></span></i> Buat Jadwal</a>
-                                    </td>
-                                </tr>
+                                @foreach($data as $d)
+                                    <tr>
+                                        <td>{{$loop->iteration}}</td>
+                                        <td>{{$d->kode_mata_kuliah}}</td>
+                                        <td>{{$d->nama_mata_kuliah}}</td>
+                                        <td>{{$d->nama_kelas_kuliah}}</td>
+                                        <td>{{$d->nama_ruang}}</td>
+                                        <td>{{$d->nama_semester}}</td>
+                                        <td>{{$d->jadwal_hari}}, {{$d->jadwal_jam_mulai}} - {{$d->jadwal_jam_selesai}}</td>
+                                    </tr>
+                                @endforeach
                             </tbody>
 					  </table>
                     </div>
@@ -64,4 +64,20 @@ Jadwal Kuliah Dosen
     </div>			
 </section>
 @endsection
+@push('js')
+
+<script>
+      $(document).ready(function() {
+        $('#data').DataTable({
+            "paging": true,
+            "ordering": true,
+            "searching": true,
+            // "scrollCollapse": true,
+            // "scrollY": "550px",
+        });
+
+    });
+</script>
+
+@endpush
 
