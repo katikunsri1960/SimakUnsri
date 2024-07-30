7bd97b719c72149310c1905912b7c9744e9b25f8
Noordin As-Shiddiq Mangkunegara <47419175+shiddiqmangkunegara@users.noreply.github.com>
2024-07-30 17:05:13 +0700

    Pembuatan tabel beasiswa mahasiswa dan jenis beasiswa mahasiswa pada database simak unsri 2024

---
+++ database/migrations/2024_07_30_151701_create_beasiswa_mahasiswas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('beasiswa_mahasiswas', function (Blueprint $table) {
+            $table->id();
+            $table->string('id_registrasi_mahasiswa')->nullable();
+            $table->foreign('id_registrasi_mahasiswa')->references('id_registrasi_mahasiswa')->on('riwayat_pendidikans')->onDelete('set null');
+            $table->string('nim');
+            $table->string('nama_mahasiswa');
+            $table->string('id_jenis_beasiswa');
+            $table->timestamps(); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('beasiswa_mahasiswas');
+    }
+};
---
+++ database/migrations/2024_07_30_163628_create_jenis_beasiswa_mahasiswas_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jenis_beasiswa_mahasiswas', function (Blueprint $table) {
+            $table->id('id_jenis_beasiswa');
+            $table->string('nama_jenis_beasiswa');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jenis_beasiswa_mahasiswas');
+    }
+};
