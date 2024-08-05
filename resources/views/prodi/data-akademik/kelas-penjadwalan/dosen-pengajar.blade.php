4f91aeb8a501c2e2a6ebc0f746bf96cc1c3d1e56
Noordin As-Shiddiq Mangkunegara <47419175+shiddiqmangkunegara@users.noreply.github.com>
2024-08-05 10:05:13 +0700

    Penambahan kolom pada tabel aktivitas mahasiswa dan pembuatan fitur ajuan sidang mahasiswa

--- resources/views/prodi/data-akademik/kelas-penjadwalan/dosen-pengajar.blade.php
+++ resources/views/prodi/data-akademik/kelas-penjadwalan/dosen-pengajar.blade.php
@@ -10,7 +10,7 @@ Dosen Kelas Perkuliahan
 <div class="content-header">
     <div class="d-flex align-items-center">
         <div class="me-auto">
-            <h3 class="page-title">Tambah Kelas Perkuliahan</h3>
+            <h3 class="page-title">Dosen Kelas Perkuliahan</h3>
             <div class="d-inline-block align-items-center">
                 <nav>
                     <ol class="breadcrumb">
