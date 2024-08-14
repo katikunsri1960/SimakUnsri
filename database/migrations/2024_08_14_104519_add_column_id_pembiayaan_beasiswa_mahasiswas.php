e5a02c2a270b18b42642b836513258d91544fc87
Noordin As-Shiddiq Mangkunegara <47419175+shiddiqmangkunegara@users.noreply.github.com>
2024-08-14 10:19:32 +0700

    Update status beasiswa pada AKM

---
+++ database/migrations/2024_08_14_104519_add_column_id_pembiayaan_beasiswa_mahasiswas.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('beasiswa_mahasiswas', function (Blueprint $table) {
+            $table->foreignId('id_pembiayaan')->after('id_jenis_beasiswa')->nullable()->constrained('jenis_beasiswa_mahasiswas')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('beasiswa_mahasiswas', function (Blueprint $table) {
+            $table->dropColumn('id_pembiayaan');
+        });
+    }
+};
