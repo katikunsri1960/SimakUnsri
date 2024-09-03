b7371b230a249e930cd29a59a93e865b04c76642
UPT TIK Unsri <ka.tik@unsri.ac.id>
2024-09-03 12:00:47 +0700

    Update TranskripController view to display student information and photo

--- resources/views/bak/transkrip/index.blade.php
+++ resources/views/bak/transkrip/index.blade.php
@@ -41,44 +41,52 @@ Transkrip Nilai
                             <div class="table-responsive">
                                 <div id="krsDiv" hidden>
                                     <h3 class="text-center">Transkrip Mahasiswa</h3>
-                                    <table style="width:100%" class="mb-3">
-                                        <tr>
-                                            <td class="text-start align-middle" style="width: 12%">NIM</td>
-                                            <td>:</td>
-                                            <td class="text-start" id="nimKrs" style="width: 45%; padding-left: 10px"></td>
-                                            <td class="text-start align-middle" style="width: 18%">FAKULTAS</td>
-                                            <td>:</td>
-                                            <td class="text-start align-middle" id="fakultasKrs"
-                                                style="width: 30%; padding-left: 10px"></td>
-                                        </tr>
-                                        <tr>
-                                            <td class="text-start align-middle" style="width: 12%">NAMA</td>
-                                            <td>:</td>
-                                            <td class="text-start" id="namaKrs" style="width: 45%; padding-left: 10px"></td>
-                                            <td class="text-start align-middle" style="width: 18%">JURUSAN</td>
-                                            <td>:</td>
-                                            <td class="text-start align-middle" id="jurusanKrs"
-                                                style="width: 30%; padding-left: 10px"></td>
-                                        </tr>
-                                        <tr>
-                                            <td class="text-start align-middle" style="width: 12%">NIP PA</td>
-                                            <td>:</td>
-                                            <td class="text-start" id="nippaKrs" style="width: 45%; padding-left: 10px"></td>
-                                            <td class="text-start align-middle" style="width: 18%">PROGRAM STUDI</td>
-                                            <td>:</td>
-                                            <td class="text-start align-middle" id="prodiKrs"
-                                                style="width: 30%; padding-left: 10px"></td>
-                                        </tr>
-                                        <tr>
-                                            <td class="text-start align-middle" style="width: 12%">DOSEN PA</td>
-                                            <td>:</td>
-                                            <td class="text-start" id="dosenpaKrs" style="width: 45%; padding-left: 10px"></td>
-                                            <td class="text-start align-middle" style="width: 18%">SEMESTER</td>
-                                            <td>:</td>
-                                            <td class="text-start align-middle" id="semesterKrs"
-                                                style="width: 30%; padding-left: 10px"></td>
-                                        </tr>
-                                    </table>
+                                    <div class="row">
+                                        <div class="col-md-2" id="foto">
+
+                                        </div>
+                                        <div class="col-md-10">
+                                            <table style="width:100%" class="mb-3">
+                                                <tr>
+                                                    <td class="text-start align-middle" style="width: 12%">NIM</td>
+                                                    <td>:</td>
+                                                    <td class="text-start" id="nimKrs" style="width: 45%; padding-left: 10px"></td>
+                                                    <td class="text-start align-middle" style="width: 18%">FAKULTAS</td>
+                                                    <td>:</td>
+                                                    <td class="text-start align-middle" id="fakultasKrs"
+                                                        style="width: 30%; padding-left: 10px"></td>
+                                                </tr>
+                                                <tr>
+                                                    <td class="text-start align-middle" style="width: 12%">NAMA</td>
+                                                    <td>:</td>
+                                                    <td class="text-start" id="namaKrs" style="width: 45%; padding-left: 10px"></td>
+                                                    <td class="text-start align-middle" style="width: 18%">JURUSAN</td>
+                                                    <td>:</td>
+                                                    <td class="text-start align-middle" id="jurusanKrs"
+                                                        style="width: 30%; padding-left: 10px"></td>
+                                                </tr>
+                                                <tr>
+                                                    <td class="text-start align-middle" style="width: 12%">NIP PA</td>
+                                                    <td>:</td>
+                                                    <td class="text-start" id="nippaKrs" style="width: 45%; padding-left: 10px"></td>
+                                                    <td class="text-start align-middle" style="width: 18%">PROGRAM STUDI</td>
+                                                    <td>:</td>
+                                                    <td class="text-start align-middle" id="prodiKrs"
+                                                        style="width: 30%; padding-left: 10px"></td>
+                                                </tr>
+                                                <tr>
+                                                    <td class="text-start align-middle" style="width: 12%">DOSEN PA</td>
+                                                    <td>:</td>
+                                                    <td class="text-start" id="dosenpaKrs" style="width: 45%; padding-left: 10px"></td>
+                                                    <td class="text-start align-middle" style="width: 18%">SEMESTER</td>
+                                                    <td>:</td>
+                                                    <td class="text-start align-middle" id="semesterKrs"
+                                                        style="width: 30%; padding-left: 10px"></td>
+                                                </tr>
+                                            </table>
+                                        </div>
+                                    </div>
+
                                     <div class="d-flex justify-content-end align-middle">
 
                                     </div>
@@ -98,6 +106,15 @@ Transkrip Nilai
                                         <tbody>
 
                                         </tbody>
+                                        <tfoot>
+                                            <tr>
+                                                <th colspan="3" class="text-center align-middle">Total SKS</th>
+                                                <th class="text-center align-middle" id="totalSks"></th>
+                                                <th ></th>
+                                                <th ></th>
+                                                <th ></th>
+                                            </tr>
+                                        </tfoot>
                                     </table>
                                     <hr>
                                     <div class="row mt-5" id="transferDiv" hidden>
@@ -117,6 +134,7 @@ Transkrip Nilai
                                             <tbody>
 
                                             </tbody>
+
                                         </table>
                                     </div>
 
@@ -185,9 +203,16 @@ Transkrip Nilai
                             $('#semesterKrs').text(semesterText);
                             $('#krs-regular tbody').empty();
 
+                            // append foto
+                            var imagePath = '{{ asset('storage') }}' + '/' + response.riwayat.angkatan + '/' + response.riwayat.nim + '.jpg';
+                            $('#foto').html(`
+                                <img class="rounded20 bg-light img-fluid w-80" src="` + imagePath + `" alt="" onerror="this.onerror=null;this.src='{{ asset('images/images/avatar/avatar-15.png') }}';">
+                            `);
+
                             // count response.krs.approved
                             var approved = 0;
                             var no = 1;
+                            var totalSks = 0;
 
                             console.log(response.konversi);
 
@@ -209,7 +234,12 @@ Transkrip Nilai
                                     </tr>
                                 `);
                                 no++;
+                                // make sks_mata_kuliah to number
+                                krs.sks_mata_kuliah = parseInt(krs.sks_mata_kuliah);
+                                totalSks += krs.sks_mata_kuliah;
                             });
+
+                            $('#totalSks').text(totalSks);
 
                         }
                     });
