0d4cc2b79cf728076e2d3e6ecb77e54973f9df3c
UPT TIK Unsri <ka.tik@unsri.ac.id>
2024-09-11 16:45:06 +0700

    Refactor FixPembayaranAkm command to improve memory management and update payment logic

--- app/Console/Commands/FixPembayaranAkm.php
+++ app/Console/Commands/FixPembayaranAkm.php
@@ -32,15 +32,16 @@ class FixPembayaranAkm extends Command
      */
     public function handle()
     {
+        ini_set('memory_limit', '2048M');
         $semester_aktif = SemesterAktif::first();
 
         $aktivitas_kuliah = AktivitasKuliahMahasiswa::where('feeder', 0)
-                            ->where('id_semester', $semester_aktif->id_semester)->get();
+                            ->where('id_semester', $semester_aktif->id_semester)->whereNull('id_pembiayaan')->get();
 
         foreach ($aktivitas_kuliah as $akm) {
             $req = $this->checkPembayaran($akm->id_registrasi_mahasiswa, $semester_aktif->id_semester);
 
-            $this->info($req['status']. ' ');
+            $this->info($req['status']. ' - '. $req['message']. ' - '. $req['data']);
         }
 
     }
@@ -52,18 +53,16 @@ class FixPembayaranAkm extends Command
                 ->where('id_registrasi_mahasiswa', $id_reg)
                 ->first();
 
-        $id_test = Registrasi::where('rm_nim', $nim->nim)->pluck('rm_no_test')->first();
-
-
         // dd($id_test);
         $beasiswa = BeasiswaMahasiswa::where('id_registrasi_mahasiswa', $id_reg)->first();
         // dd($beasiswa);
-        $akm = AktivitasKuliahMahasiswa::where('id_registrasi_mahasiswa', $id_reg)->first();
+        $akm = AktivitasKuliahMahasiswa::where('id_registrasi_mahasiswa', $id_reg)->where('id_semester', $semester_aktif)->where('feeder', 0)->first();
 
         if($beasiswa){
 
             $akm->update([
                 'id_pembiayaan' => 2,
+                'biaya_kuliah_smt' => 0,
             ]);
 
             return [
@@ -73,14 +72,31 @@ class FixPembayaranAkm extends Command
             ];
         }
 
+        $id_test = Registrasi::where('rm_nim', $nim->nim)->pluck('rm_no_test')->first();
+
         $tagihan = Tagihan::with('pembayaran')
-                    ->whereIn('tagihan.nomor_pembayaran', [$id_test, $nim])
+                    ->whereIn('tagihan.nomor_pembayaran', [$id_test, $nim->nim])
                     ->where('tagihan.kode_periode', $semester_aktif)
                     ->first();
 
         if ($tagihan) {
+            $akm->update([
+                'id_pembiayaan' => 1,
+                'biaya_kuliah_smt' => $tagihan->total_nilai_tagihan,
+            ]);
 
+            return [
+                'status' => 'success',
+                'message' => 'Tagihan',
+                'data' => $nim->nim.' - '.$nim->nama_mahasiswa
+            ];
         }
+
+        return [
+            'status' => 'error',
+            'message' => 'Tidak ada pembayaran',
+            'data' => $nim->nim.' - '.$nim->nama_mahasiswa
+        ];
 
     }
 }
