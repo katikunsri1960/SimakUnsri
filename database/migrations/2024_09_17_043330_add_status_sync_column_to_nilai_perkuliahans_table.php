775bab9baed98d92447a12df9a6f3180bc9240e9
UPT TIK Unsri <ka.tik@unsri.ac.id>
2024-09-17 04:34:03 +0700

    Add status_sync column to nilai_perkuliahans table

---
+++ database/migrations/2024_09_17_043330_add_status_sync_column_to_nilai_perkuliahans_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('nilai_perkuliahans', function (Blueprint $table) {
+            $table->string('status_sync')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('nilai_perkuliahans', function (Blueprint $table) {
+            $table->dropColumn('status_sync');
+        });
+    }
+};
