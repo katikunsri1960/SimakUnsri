a5438a4bbbbaa53314406ff970583114a8dbd443
Noordin As-Shiddiq Mangkunegara <47419175+shiddiqmangkunegara@users.noreply.github.com>
2024-10-14 12:09:55 +0700

    Ubah cara menampilkan nama dan nim mahasiswa

--- resources/views/prodi/data-akademik/non-tugas-akhir/edit.blade.php
+++ resources/views/prodi/data-akademik/non-tugas-akhir/edit.blade.php
@@ -72,7 +72,7 @@ Tugas Akhir
                                     name="nim"
                                     id="nim"
                                     aria-describedby="helpId"
-                                    value="{{$d->anggota_aktivitas_personal->nim}}"
+                                    value="{{$d->anggota_aktivitas_personal ? $d->anggota_aktivitas_personal->nim : '-'}}"
                                     disabled
                                 />
                             </div>
@@ -86,7 +86,7 @@ Tugas Akhir
                                     name="nama_mahasiswa"
                                     id="nama_mahasiswa"
                                     aria-describedby="helpId"
-                                    value="{{$d->anggota_aktivitas_personal->nama_mahasiswa}}"
+                                    value="{{$d->anggota_aktivitas_personal ? $d->anggota_aktivitas_personal->nama_mahasiswa : '-'}}"
                                     disabled
                                 />
                             </div>
