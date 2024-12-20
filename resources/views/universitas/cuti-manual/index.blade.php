0c2cae3eb9ce072954283a5804639544b557444f
Ryan Fitra Perdana <36030822+ryanfitra@users.noreply.github.com>
2024-12-20 15:52:29 +0700

    update edit batas isi krs universitas

--- resources/views/universitas/cuti-manual/index.blade.php
+++ resources/views/universitas/cuti-manual/index.blade.php
@@ -130,7 +130,7 @@ $(document).ready(function() {
         width: '100%',
         minimumInputLength: 3,
         ajax: {
-            url: "{{route('univ.pengaturan.akun.get-mahasiswa')}}",
+            url: "{{route('univ.cuti-manual.get-mahasiswa')}}",
             type: "GET",
             dataType: 'json',
             delay: 250,
