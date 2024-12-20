a2341ae1fb003471a041c83a3be48250d4ce43d6
UPT TIK Unsri <ka.tik@unsri.ac.id>
2024-12-20 16:51:12 +0700

    Refactor evaluation component logic and improve validation checks in PresentasePenilaianController

--- app/Http/Controllers/Dosen/Penilaian/PresentasePenilaianController.php
+++ app/Http/Controllers/Dosen/Penilaian/PresentasePenilaianController.php
@@ -50,14 +50,14 @@ class PresentasePenilaianController extends Controller
                 }
             }
         }else{
-            $bobot_participatory = 0; 
+            $bobot_participatory = 0;
             $bobot_project = 0;
             $bobot_assignment = 0;
             $bobot_quiz = 0;
             $bobot_midterm = 0;
             $bobot_finalterm = 0;
         }
-        
+
         // dd($data_komponen);
         return view('dosen.penilaian.presentase.komponen-evaluasi', [
             'data' => $data_komponen,
@@ -94,13 +94,14 @@ class PresentasePenilaianController extends Controller
             'midterm_exam' => 'required',
             'finalterm_exam' => 'required'
         ]);
-
+        $data_kelas = KelasKuliah::with(['matkul', 'prodi'])->where('id_kelas_kuliah', $kelas)->first();
         //Check komponen kelas kuliah masih kosong
         $komponen_kelas = KomponenEvaluasiKelas::where('id_kelas_kuliah', $kelas)->get();
+        $prodi_bebas_jadwal = ['12201','11201','14201','11706', '11707', '11708', '11711', '11718', '11702', '11704', '11701', '11703', '11705', '11728', '11735', '12901', '11901', '14901', '23902', '86904', '48901'];
 
         // dd($interval);
 
-        if($komponen_kelas->isEmpty() && (date('Y-m-d') <= $semester_aktif->batas_isi_nilai && !in_array($kelas->prodi->kode_program_studi, $prodi_bebas_jadwal))){
+        if($komponen_kelas->isEmpty() && (date('Y-m-d') <= $semester_aktif->batas_isi_nilai && !in_array($data_kelas->prodi->kode_program_studi, $prodi_bebas_jadwal))){
             //Check jumlah bobot komponen evaluasi
             $total_bobot_input = $request->participatory + $request->project_outcomes + $request->assignment + $request->quiz + $request->midterm_exam + $request->finalterm_exam;
 
@@ -138,11 +139,11 @@ class PresentasePenilaianController extends Controller
 
                 //Store data finalterm
                 KomponenEvaluasiKelas::create(['feeder'=> 0, 'id_komponen_evaluasi'=> $id_komp_eval6, 'id_kelas_kuliah'=> $kelas, 'id_jenis_evaluasi'=> 4,  'nama'=> 'UAS', 'nama_inggris'=> 'Finalterm Exam', 'nomor_urut'=> 6, 'bobot_evaluasi'=> $bobot_finalterm]);
-                
+
                 return redirect()->back()->with('success', 'Data Berhasil di Tambahkan');
             }else{
                 return redirect()->back()->with('error', 'Bobot Komponen Evaluasi Tidak Sama Dengan 100%');
-            }            
+            }
         }else{
             return redirect()->back()->with('error', 'Komponen Evaluasi Telah di Buat / Cek Batas Pengisian Nilai');
         }
@@ -173,7 +174,7 @@ class PresentasePenilaianController extends Controller
         ]);
 
         $prodi_not_scheduled = ['12201','11201','14201','11706', '11707', '11708', '11711', '11718', '11702', '11704', '11701', '11703', '11705', '11728', '11735', '12901', '11901', '14901', '23902', '86904', '48901'];
-        
+
         if(collect($komponen_kelas)->pluck('feeder')->contains('0') && (date('Y-m-d') <= $semester_aktif->batas_isi_nilai || in_array($data_kelas->prodi->kode_program_studi, $prodi_not_scheduled))){
             //Check jumlah bobot komponen evaluasi
             $total_bobot_input = $request->participatory + $request->project_outcomes + $request->assignment + $request->quiz + $request->midterm_exam + $request->finalterm_exam;
@@ -217,11 +218,11 @@ class PresentasePenilaianController extends Controller
                 KomponenEvaluasiKelas::where('id_komponen_evaluasi', $komponen_kelas[5]->id_komponen_evaluasi)
                                     ->where('id_kelas_kuliah', $kelas)
                                     ->update(['bobot_evaluasi'=> $bobot_finalterm]);
-                
+
                 return redirect()->back()->with('success', 'Data Berhasil di Update');
             }else{
                 return redirect()->back()->with('error', 'Bobot Komponen Evaluasi Tidak Sama Dengan 100%');
-            } 
+            }
         }else{
             return redirect()->back()->with('error', 'Komponen Evaluasi Telah di Sinkronisasi / Cek Batas Pengisian Nilai');
         }
