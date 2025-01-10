d0c2b29d6f75e744af7ea8012b56ca3868ed3136
UPT TIK Unsri <ka.tik@unsri.ac.id>
2025-01-10 23:23:07 +0700

    perbaikan approve krs

--- app/Models/Perkuliahan/PesertaKelasKuliah.php
+++ app/Models/Perkuliahan/PesertaKelasKuliah.php
@@ -10,6 +10,7 @@ use App\Models\Mahasiswa\RiwayatPendidikan;
 use App\Models\Connection\Registrasi;
 use App\Models\BeasiswaMahasiswa;
 use App\Models\Connection\Tagihan;
+use App\Models\PembayaranManualMahasiswa;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -782,7 +783,6 @@ class PesertaKelasKuliah extends Model
 
         $data_mbkm_eksternal =  $krs_aktivitas_mbkm->count();
 
-
         list($krs_akt, $data_akt_ids) = $db_akt->getKrsAkt($id_reg, $semester_aktif->id_semester);
 
         $sks_max = $db->getSksMax($id_reg, $semester_aktif->id_semester, $riwayat_pendidikan->id_periode_masuk);
@@ -815,7 +815,6 @@ class PesertaKelasKuliah extends Model
                     'feeder' => 0
                 ]);
             }
-
 
             foreach ($aktivitas as $item) {
 
@@ -1476,5 +1475,224 @@ class PesertaKelasKuliah extends Model
 
         return $result;
 
+    }
+
+    public function approve_v2($id_reg)
+    {
+        $semester_aktif = SemesterAktif::first();
+        $db_akt = new AktivitasMahasiswa();
+
+        $data = PesertaKelasKuliah::with(['kelas_kuliah', 'kelas_kuliah.matkul'])
+                ->whereHas('kelas_kuliah', function($query) use ($semester_aktif) {
+                    $query->where('id_semester', $semester_aktif->id_semester);
+                })
+                ->where('id_registrasi_mahasiswa', $id_reg)
+                ->where('submitted', 1)
+                ->orderBy('kode_mata_kuliah')
+                ->get();
+
+        $aktivitas = $db_akt->with('anggota_aktivitas_personal', 'konversi')
+                ->whereHas('anggota_aktivitas_personal', function($query) use ($id_reg) {
+                    $query->where('id_registrasi_mahasiswa', $id_reg);
+                })
+                ->where('id_semester', $semester_aktif->id_semester)
+                ->where('submitted', 1)
+                ->get();
+
+
+        // Lakukan Pengecekan terlebih dahulu agar query di bawah tidak perlu dijalankan jika data krs kosong atau mahasiswa blm submit krs.
+        if(count($data) == 0 && count($aktivitas) == 0){
+            $result = [
+                'status' => 'error',
+                'message' => 'Mahasiswa belum submit KRS final.',
+            ];
+
+            return $result;
+        }
+
+        $riwayat_pendidikan = RiwayatPendidikan::select('riwayat_pendidikans.*', 'biodata_dosens.id_dosen', 'biodata_dosens.nama_dosen')
+                ->where('id_registrasi_mahasiswa', $id_reg)
+                ->leftJoin('biodata_dosens', 'biodata_dosens.id_dosen', '=', 'riwayat_pendidikans.dosen_pa')
+                ->first();
+
+        $transkrip = TranskripMahasiswa::select(
+                    DB::raw('SUM(CAST(sks_mata_kuliah AS UNSIGNED)) as total_sks'), // Mengambil total SKS tanpa nilai desimal
+                    DB::raw('ROUND(SUM(nilai_indeks * sks_mata_kuliah) / SUM(sks_mata_kuliah), 2) as ipk') // Mengambil IPK dengan 2 angka di belakang koma
+                )
+                ->where('id_registrasi_mahasiswa', $id_reg)
+                ->whereNotIn('nilai_huruf', ['F', ''])
+                ->groupBy('id_registrasi_mahasiswa')
+                ->first();
+
+        if ((!$transkrip || $transkrip->ipk === null) && $riwayat_pendidikan->id_periode_masuk != $semester_aktif->id_semester) {
+            return [
+                'status' => 'error',
+                'message' => 'Transkrip Mahasiswa ini belum di cheklist!! Harap menghubungi Admin Program Studi untuk melakukan perbaikan data!!',
+            ];
+        }
+
+        $db = new MataKuliah();
+
+        $jalur_pendaftaran = $riwayat_pendidikan->id_jenis_daftar;
+        // dd($jalur_pendaftaran);
+
+        $data_mbkm = PesertaKelasKuliah::with(['kelas_kuliah', 'kelas_kuliah.matkul'])
+                    ->whereHas('kelas_kuliah', function($query) use ($semester_aktif,$riwayat_pendidikan) {
+                        $query->where('id_semester', $semester_aktif->id_semester)->whereNot('id_prodi', $riwayat_pendidikan->id_prodi);
+                    })
+                    ->where('id_registrasi_mahasiswa', $id_reg)
+                    ->orderBy('kode_mata_kuliah')
+                    ->count();
+
+        $beasiswa = BeasiswaMahasiswa::where('id_registrasi_mahasiswa', $id_reg)->first();
+
+        $total_nilai_tagihan = 0;
+
+        try{
+
+            $id_test = Registrasi::where('rm_nim', $riwayat_pendidikan->nim)->pluck('rm_no_test')->first();
+            $tagihan = Tagihan::with('pembayaran')
+                    ->whereIn('nomor_pembayaran', [$id_test, $riwayat_pendidikan->nim])
+                    ->where('kode_periode', $semester_aktif->id_semester
+                    // -1
+                    )
+                    ->first();
+
+            // Check if tagihan is null or total_nilai_tagihan == 0 ? 0 ? $total_nilai_tagihan is null, and set to 0
+            $total_nilai_tagihan = !$tagihan || $tagihan->total_nilai_tagihan == NULL ? 0 : $tagihan->total_nilai_tagihan;
+
+        }catch (\Exception $e) {
+            $result = [
+                'status' => 'error',
+                'message' => 'Koneksi Database Keuangan Terputus!!'
+            ];
+
+            return $result;
+        }
+
+
+        $krs_aktivitas_mbkm = AktivitasMahasiswa::with(['anggota_aktivitas'])
+                    ->whereHas('anggota_aktivitas' , function($query) use ($id_reg) {
+                            $query->where('id_registrasi_mahasiswa', $id_reg);
+                    })
+                    // ->where('approve_krs', 1)
+                    ->where('id_semester', $semester_aktif->id_semester)
+                    ->where('submitted', 1)
+                    ->whereIn('id_jenis_aktivitas',['13','14','15','16','17','18','19','20', '21'])
+                    ->get();
+
+        $data_mbkm_eksternal =  $krs_aktivitas_mbkm->count();
+
+        list($krs_akt, $data_akt_ids) = $db_akt->getKrsAkt($id_reg, $semester_aktif->id_semester);
+
+        $sks_max = $db->getSksMax($id_reg, $semester_aktif->id_semester, $riwayat_pendidikan->id_periode_masuk);
+
+        $krs_regular = $db->getKrsRegular($id_reg, $riwayat_pendidikan, $semester_aktif->id_semester, $data_akt_ids);
+
+        $krs_merdeka = $db->getKrsMerdeka($id_reg, $semester_aktif->id_semester, $riwayat_pendidikan->id_prodi);
+
+        $total_sks_akt = $krs_akt->sum('konversi.sks_mata_kuliah');
+        $total_sks_merdeka = $krs_merdeka->sum('sks_mata_kuliah');
+        $total_sks_regular = $krs_regular->sum('sks_mata_kuliah');
+        $total_sks_mbkm = $krs_aktivitas_mbkm->sum('sks_aktivitas');
+
+        $total_sks = $total_sks_regular + $total_sks_merdeka + $total_sks_akt + $total_sks_mbkm;
+
+        $status_akm = [
+            'id_status_mahasiswa' => $riwayat_pendidikan->id_jenis_daftar == '14' || $data_mbkm > 0 || $data_mbkm_eksternal > 0 ? 'M' : 'A',
+            'nama_status_mahasiswa' => $riwayat_pendidikan->id_jenis_daftar == '14' || $data_mbkm > 0 || $data_mbkm_eksternal > 0  ? 'Kampus Merdeka' : 'Aktif',
+        ];
+
+        $checkPembayaranManual = PembayaranManualMahasiswa::where('id_registrasi_mahasiswa', $id_reg)->where('id_semester', $semester_aktif->id_semester)->first();
+
+        if ($beasiswa && $beasiswa->tanggal_akhir_beasiswa > now()) {
+            $bayar = [
+                'id_pembiayaan' => $beasiswa->id_pembiayaan,
+                'biaya_kuliah_smt' => $total_nilai_tagihan,
+            ];
+        } else if ($tagihan) {
+            $bayar = [
+                'id_pembiayaan' => 1,
+                'biaya_kuliah_smt' => $checkPembayaranManual ? $checkPembayaranManual->nominal_ukt : $tagihan->total_nilai_tagihan,
+            ];
+        } else if ($jalur_pendaftaran == '14') {
+            // pengecekan apakah jalur pendaftaran == 14 dimana adalah (Pendidikan non gelar)
+            $bayar = [
+                'id_pembiayaan' => 3,
+                'biaya_kuliah_smt' => 0,
+            ];
+        } else {
+
+            $message = $beasiswa ? "Beasiswa sudah melewati masa berlakunya! Harap menghubungi BAK dan Admin Universitas untuk informasi lebih lanjut!" : "Data tidak terdata didalam tagihan ataupun beasiswa!";
+
+            $result = [
+                'status' => 'error',
+                'message' => $message,
+            ];
+
+            return $result;
+        }
+
+
+        try {
+
+            DB::beginTransaction();
+
+
+            foreach ($aktivitas as $item) {
+                $item->update([
+                    'approve_krs' => '1',
+                    'tanggal_approve' => date('Y-m-d')
+                ]);
+            }
+
+            foreach ($data as $item) {
+                $item->update([
+                    'approved' => '1',
+                    'tanggal_approve' => date('Y-m-d')
+                ]);
+            }
+
+
+            AktivitasKuliahMahasiswa::updateOrCreate(['id_registrasi_mahasiswa' => $id_reg, 'id_semester' => $semester_aktif->id_semester], [
+                'feeder' => 0,
+                'nim' => $riwayat_pendidikan->nim,
+                'nama_mahasiswa' => $riwayat_pendidikan->nama_mahasiswa,
+                'id_prodi' => $riwayat_pendidikan->id_prodi,
+                'nama_program_studi' => $riwayat_pendidikan->nama_program_studi,
+                'angkatan' => $riwayat_pendidikan->periode_masuk->id_tahun_ajaran,
+                'id_periode_masuk' => $riwayat_pendidikan->id_periode_masuk,
+                'nama_semester' => $semester_aktif->semester->nama_semester,
+                'ips' => '0.00',
+                'ipk'=> !$transkrip && $riwayat_pendidikan->id_periode_masuk == $semester_aktif->id_semester ? 0 : $transkrip->ipk,
+                'sks_semester'=> $total_sks,
+                'sks_total'=> !$transkrip && $riwayat_pendidikan->id_periode_masuk == $semester_aktif->id_semester ? $total_sks : $transkrip->total_sks+$total_sks,
+                'id_status_mahasiswa' => $status_akm['id_status_mahasiswa'],
+                'nama_status_mahasiswa' => $status_akm['nama_status_mahasiswa'],
+                'biaya_kuliah_smt' => $bayar['biaya_kuliah_smt'],
+                'id_pembiayaan' => $bayar['id_pembiayaan'],
+                'status_sync' => 'belum sync',
+            ]);
+
+            DB::commit();
+
+            $result = [
+                'status' => 'success',
+                'message' => 'Semua data berhasil disetujui!',
+            ];
+
+        } catch (\Exception $e) {
+
+            DB::rollBack();
+
+            $result = [
+                'status' => 'error',
+                'message' => 'Terjadi kesalahan!'. $e->getMessage(),
+            ];
+
+            return $result;
+        }
+
+        return $result;
     }
 }
