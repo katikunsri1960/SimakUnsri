55fce46ee26b0eb66224b4d1affcda0d6f4efe95
Ryan Fitra Perdana <36030822+ryanfitra@users.noreply.github.com>
2025-01-31 20:57:36 +0700

    update error untuk khs mahasiswa dengan prodi alih bentu

--- app/Http/Controllers/Fakultas/Akademik/KHSController.php
+++ app/Http/Controllers/Fakultas/Akademik/KHSController.php
@@ -41,7 +41,10 @@ class KHSController extends Controller
         $semester = $request->semester;
         $nim = $request->nim;
 
-        $riwayat = RiwayatPendidikan::with('dosen_pa', 'prodi.jurusan', 'prodi.fakultas')
+        $riwayat = RiwayatPendidikan::with('dosen_pa','prodi', 'prodi.jurusan', 'prodi.fakultas')
+                    ->whereHas('prodi', function($query) {
+                            $query->where('status', 'A');
+                        })
                     ->where('nim', $nim)
                     ->whereIn('id_prodi', $id_prodi_fak)
                     ->first();
@@ -53,7 +56,8 @@ class KHSController extends Controller
             ]);
         }
 
-        $nilai = NilaiPerkuliahan::with('semester')->where('id_registrasi_mahasiswa', $riwayat->id_registrasi_mahasiswa)
+        $nilai = NilaiPerkuliahan::with('semester')
+                ->where('id_registrasi_mahasiswa', $riwayat->id_registrasi_mahasiswa)
                 ->where('id_semester', $semester)
                 ->orderBy('id_semester')
                 ->get();
