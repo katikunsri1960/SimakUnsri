bc84bbaa0b516d385568d7a6277ccd50d4bc8058
UPT TIK Unsri <ka.tik@unsri.ac.id>
2025-02-24 12:31:18 +0700

    Enhance participant view with filtering options for Fakultas, Prodi, and Periode; integrate DataTables and Select2 for improved user experience

--- resources/views/bak/wisuda/peserta/index.blade.php
+++ resources/views/bak/wisuda/peserta/index.blade.php
@@ -24,30 +24,86 @@ Daftar Peserta Wisuda
     <div class="row">
         <div class="col-12">
             <div class="box box-outline-success bs-3 border-success">
-                {{-- <div class="box-header with-border">
-                    <div class="d-flex justify-content-end">
-                        <button type="button" class="btn btn-primary waves-effect waves-light" data-bs-toggle="modal"
-                        data-bs-target="#createModal">
-                            <i class="fa fa-plus"></i> Tambah Periode
-                        </button>
-                        <span class="divider-line mx-1"></span>
+                <div class="box-header with-border">
+                    <div class="form-group row">
+                        <label class="col-form-label col-md-2">Fakultas</label>
+                        <div class="col-md-8">
+                            <select name="fakultas" id="fakultas" required class="form-select" onchange="filterProdi()">
+                                <option value="*">-- Semua Fakultas --</option>
+                                @foreach ($fakultas as $f)
+                                <option value="{{$f->id}}">{{$f->nama_fakultas}}</option>
+                                @endforeach
+                            </select>
+                        </div>
                     </div>
-                </div> --}}
+                    <div class="form-group row">
+                        <label class="col-form-label col-md-2">Prodi</label>
+                        <div class="col-md-8">
+                            <select name="prodi" id="prodi" required class="form-select">
+                                <option value="*">-- Semua Prodi --</option>
+                                @foreach ($prodi as $p)
+                                <option value="{{$p->id_prodi}}">({{$p->kode_program_studi}}) - {{$p->nama_jenjang_pendidikan}} {{$p->nama_program_studi}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-form-label col-md-2">Periode</label>
+                        <div class="col-md-3">
+                            <select name="periode" id="periode" required class="form-select">
+                                @foreach ($periode as $per)
+                                <option value="{{$per->periode}}">{{$per->periode}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-form-label col-md-2">&nbsp;</label>
+                        <div class="col-md-8">
+                           <div class="row mx-2">
+                            <button class="btn btn-sm btn-primary" onclick="getData()">Tampilkan <i class="fa fa-magnifying-glass ms-2"></i></button>
+                           </div>
+                        </div>
+                    </div>
+                </div>
                 <div class="box-body">
                     <div class="table-responsive">
                         <table id="data" class="table table-bordered table-hover margin-top-10 w-p100">
                             <thead>
                                 <tr>
-                                    <th class="text-center align-middle">No</th>
-                                    <th class="text-center align-middle">Periode</th>
-                                    <th class="text-center align-middle">Tanggal Wisuda</th>
-                                    <th class="text-center align-middle">Tanggal Mulai Pendaftaran</th>
-                                    <th class="text-center align-middle">Tanggal Akhir Pendaftaran</th>
-                                    <th class="text-center align-middle">Apa Aktif</th>
-                                    <th class="text-center align-middle">ACT</th>
+                                    <th class="text-center align-middle">NO</th>
+                                    <th class="text-center align-middle">PERIODE</th>
+                                    <th class="text-center align-middle">STATUS</th>
+                                    <th class="text-center align-middle">IJAZAH TERAKHIR</th>
+                                    <th class="text-center align-middle">BERKAS REGISTRASI WISUDA</th>
+                                    <th class="text-center align-middle">NOMOR REGISTRASI</th>
+                                    <th class="text-center align-middle">FOTO</th>
+                                    <th class="text-center align-middle">FAKULTAS</th>
+                                    <th class="text-center align-middle">PROGRAM STUDI</th>
+                                    <th class="text-center align-middle">JENJANG</th>
+                                    <th class="text-center align-middle">NIM</th>
+                                    <th class="text-center align-middle">NAMA</th>
+                                    <th class="text-center align-middle">NIK</th>
+                                    <th class="text-center align-middle">TEMPAT KULIAH</th>
+                                    <th class="text-center align-middle">JALUR MASUK</th>
+                                    <th class="text-center align-middle">TEMPAT LAHIR</th>
+                                    <th class="text-center align-middle">TANGGAL LAHIR</th>
+                                    <th class="text-center align-middle">IPK</th>
+                                    <th class="text-center align-middle">ALAMAT</th>
+                                    <th class="text-center align-middle">TELP</th>
+                                    <th class="text-center align-middle">EMAIL</th>
+                                    <th class="text-center align-middle">NAMA ORANG TUA</th>
+                                    <th class="text-center align-middle">ALAMAT ORANG TUA</th>
+                                    <th class="text-center align-middle">TANGGAL MASUK</th>
+                                    <th class="text-center align-middle">TANGGAL YUDISIUM</th>
+                                    <th class="text-center align-middle">MASA STUDI</th>
+                                    <th class="text-center align-middle">JUDUL TUGAS AKHIR / THESIS / DISERTASI</th>
+                                    <th class="text-center align-middle">SCOR USEPT</th>
                                 </tr>
                             </thead>
+                            <tbody>
 
+                            </tbody>
                         </table>
                     </div>
                 </div>
@@ -58,7 +114,50 @@ Daftar Peserta Wisuda
 </section>
 @endsection
 @push('js')
+<script src="{{asset('assets/vendor_components/datatable/datatables.min.js')}}"></script>
+<script src="{{asset('assets/vendor_components/sweetalert/sweetalert.min.js')}}"></script>
+<script src="{{asset('assets/vendor_components/select2/dist/js/select2.min.js')}}"></script>
 <script>
+function getData()
+{
+    console.log('get data');
+}
 
+function filterProdi()
+{
+    var prodi = @json($prodi);
+    var fakultas = $('#fakultas').val();
+
+    if (fakultas == '*') {
+        $('#prodi').empty();
+        $('#prodi').append('<option value="*">-- Semua Prodi --</option>');
+        $.each(prodi, function (i, p) {
+            $('#prodi').append('<option value="'+p.id_prodi+'">('+p.kode_program_studi+') - '+p.nama_jenjang_pendidikan+' '+p.nama_program_studi+'</option>');
+        });
+        return;
+
+    }
+
+    var filteredProdi = prodi.filter(function (p) {
+        return p.fakultas_id == fakultas;
+    });
+
+    $('#prodi').empty();
+
+    $('#prodi').append('<option value="*">-- Semua Prodi --</option>');
+    $.each(filteredProdi, function (i, p) {
+        $('#prodi').append('<option value="'+p.id_prodi+'">('+p.kode_program_studi+') - '+p.nama_jenjang_pendidikan+' '+p.nama_program_studi+'</option>');
+    });
+
+}
+
+$(function () {
+    // "use strict";
+    $('#data').DataTable();
+
+    $('#fakultas').select2();
+    $('#prodi').select2();
+    $('#periode').select2();
+});
 </script>
 @endpush
