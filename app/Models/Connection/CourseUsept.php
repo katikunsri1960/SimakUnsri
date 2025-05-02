209c468f693df43131f50f8a9483c8dcd23a3a96
Noordin As-Shiddiq Mangkunegara <47419175+shiddiqmangkunegara@users.noreply.github.com>
2025-05-02 15:01:11 +0700

    Perbaikan nilai course usept untuk ajuan sidang

--- app/Models/Connection/CourseUsept.php
+++ app/Models/Connection/CourseUsept.php
@@ -57,31 +57,26 @@ class CourseUsept extends Model
 
     public function KonversiNilaiUsept($nilai_huruf,$nilai_angka)
     {
-        // dd($nilai_angka);
+        // $nilai_angka = str_replace(",",".",$nilai_angka);
+
         // if($nilai_huruf == 'A'){
-        //     $nilai_hasil_course = ((525-476)/(100-86)) * ($nilai_angka - 86) + 476;
+        //     $nilai_hasil_course = ((525-500)/(100-86)) * ($nilai_angka - 86) + 500;
         // }
         // else if($nilai_huruf == 'B'){
-        //     $nilai_hasil_course = ((475-375)/(85.99-71)) * ($nilai_angka - 71) + 375;
+        //     $nilai_hasil_course = ((499-450)/(85.99-71)) * ($nilai_angka - 71) + 450;
         // }
         // else{
-        //     $nilai_hasil_course = 0;
+        //     $nilai_hasil_course = 300;
         // }
 
-        // return round($nilai_hasil_course,0);
+        $nilai_angka = $this->convertToFloat($this->total_score);
 
-        // dd($nilai_angka);
-
-        $nilai_angka = str_replace(",",".",$nilai_angka);
-
-        if($nilai_huruf == 'A'){
-            $nilai_hasil_course = ((525-500)/(100-86)) * ($nilai_angka - 86) + 500;
-        }
-        else if($nilai_huruf == 'B'){
-            $nilai_hasil_course = ((499-450)/(85.99-71)) * ($nilai_angka - 71) + 450;
-        }
-        else{
-            $nilai_hasil_course = 300;
+        if ($nilai_angka >= 86 && $nilai_angka <= 100) {
+            $nilai_hasil_course = round((((525 - 500) / (100 - 86)) * ($nilai_angka - 86) + 500), 0);
+        } elseif ($nilai_angka >= 71 && $nilai_angka < 86) {
+            $nilai_hasil_course = round((((500 - 450) / (85.99 - 71)) * ($nilai_angka - 71) + 450), 0);
+        } else {
+            $nilai_hasil_course = 'Tidak Lulus'; // Default nilai untuk di bawah 71
         }
 
         return round($nilai_hasil_course,0);
