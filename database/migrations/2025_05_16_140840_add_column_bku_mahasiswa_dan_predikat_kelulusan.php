f05dbc09638b20c7aaaa986285e00dfbae00e7a2
UPT TIK Unsri <ka.tik@unsri.ac.id>
2025-05-20 12:22:55 +0700

    Refactor migration to use foreignId for id_bku_prodi and id_predikat_kelulusan in data_wisuda table

--- database/migrations/2025_05_16_140840_add_column_bku_mahasiswa_dan_predikat_kelulusan.php
+++ database/migrations/2025_05_16_140840_add_column_bku_mahasiswa_dan_predikat_kelulusan.php
@@ -14,13 +14,12 @@ return new class extends Migration
         Schema::table('data_wisuda', function (Blueprint $table) {
             // Add only if not exists
             if (!Schema::hasColumn('data_wisuda', 'id_bku_prodi')) {
-                $table->integer('id_bku_prodi')->nullable()->after('abstrak_file');
-                $table->foreign('id_bku_prodi')->references('id')->on('bku_program_studis')->onDelete('cascade');
+
+                $table->foreignId('id_bku_prodi')->nullable()->after('abstrak_file')->constrained('bku_program_studis')->nullOnDelete();
             }
 
             if (!Schema::hasColumn('data_wisuda', 'id_predikat_kelulusan')) {
-                $table->integer('id_predikat_kelulusan')->nullable()->after('id_bku_prodi');
-                $table->foreign('id_predikat_kelulusan')->references('id')->on('predikat_kelulusans')->onDelete('cascade');
+                $table->foreignId('id_predikat_kelulusan')->nullable()->after('id_bku_prodi')->constrained('predikat_kelulusans')->nullOnDelete();
             }
         });
     }
