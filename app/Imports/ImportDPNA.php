a713fc51710232b22d6f3b482662d6cc06d8b922
UPT TIK Unsri <ka.tik@unsri.ac.id>
2025-05-27 15:46:53 +0700

    Fix nilai_angka assignment in ImportDPNA: ensure proper float conversion and remove unnecessary formatting.

--- app/Imports/ImportDPNA.php
+++ app/Imports/ImportDPNA.php
@@ -152,6 +152,7 @@ class ImportDPNA implements ToCollection, WithHeadingRow, WithCalculatedFormulas
                 // }
 
                 $nilai_perkuliahan = NilaiPerkuliahan::where('id_kelas_kuliah', $this->kelas)->where('id_registrasi_mahasiswa', $mahasiswa_kelas->id_registrasi_mahasiswa)->first();
+                $nilai_angka_ = floatval(str_replace(',','.', trim($row['nilai_angka'])));
                 $nilai_indeks = floatval(str_replace(',','.', trim($row['nilai_indeks'])));
 
                 if ($nilai_perkuliahan) {
@@ -159,7 +160,7 @@ class ImportDPNA implements ToCollection, WithHeadingRow, WithCalculatedFormulas
                         ->where('id_registrasi_mahasiswa', $mahasiswa_kelas->id_registrasi_mahasiswa)
                         ->update([
                             'feeder' => 0,
-                            'nilai_angka' => number_format($row['nilai_angka'], 2),
+                            'nilai_angka' => $nilai_angka_,
                             'nilai_indeks' => $nilai_indeks,
                             'nilai_huruf' => $row['nilai_huruf'],
                         ]);
@@ -182,7 +183,7 @@ class ImportDPNA implements ToCollection, WithHeadingRow, WithCalculatedFormulas
                         'nama_mahasiswa' => $row['nama_mahasiswa'],
                         'jurusan' => $mahasiswa_kelas->nama_program_studi,
                         'angkatan' => $mahasiswa_kelas->angkatan,
-                        'nilai_angka' => number_format($row['nilai_angka'], 2),
+                        'nilai_angka' => $nilai_angka_,
                         'nilai_indeks' => $nilai_indeks,
                         'nilai_huruf' => $row['nilai_huruf'],
                     ]);
