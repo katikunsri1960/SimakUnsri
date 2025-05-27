732e744e7f82dfb10a038300283cd339b6bde67d
UPT TIK Unsri <ka.tik@unsri.ac.id>
2025-05-27 15:40:10 +0700

    Fix nilai_indeks assignment in ImportDPNA: correct array access for nilai_indeks extraction.

--- app/Imports/ImportDPNA.php
+++ app/Imports/ImportDPNA.php
@@ -152,7 +152,7 @@ class ImportDPNA implements ToCollection, WithHeadingRow, WithCalculatedFormulas
                 // }
 
                 $nilai_perkuliahan = NilaiPerkuliahan::where('id_kelas_kuliah', $this->kelas)->where('id_registrasi_mahasiswa', $mahasiswa_kelas->id_registrasi_mahasiswa)->first();
-                $nilai_indeks = floatval(str_replace(',','.', trim($row('nilai_indeks'))));
+                $nilai_indeks = floatval(str_replace(',','.', trim($row['nilai_indeks'])));
 
                 if ($nilai_perkuliahan) {
                     NilaiPerkuliahan::where('id_kelas_kuliah', $this->kelas)
