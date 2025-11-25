7fe7912f7dcc9f48c7ad0b86ea411e3eee22e6f0
ryan_fitra <ryanfitraperdana@gmail.com>
2025-11-25 08:55:54 +0700

    update referensiController

--- app/Http/Controllers/Universitas/ReferensiController.php
+++ app/Http/Controllers/Universitas/ReferensiController.php
@@ -1,79 +1,172 @@
 <?php
 
-namespace App\Services\Feeder;
+namespace App\Http\Controllers\Universitas;
 
-use GuzzleHttp\Client;
+use App\Models\ProgramStudi;
+use App\Models\Wilayah;
+use App\Models\LevelWilayah;
+use App\Models\Negara;
+use App\Services\Feeder\FeederAPI;
+use App\Http\Controllers\Controller;
+use App\Models\Referensi\AllPt;
+use Illuminate\Http\Request;
 
-class FeederAPI {
-    // url Feeder Dikti
-    private $url;
-    // Username Feeder Dikti
-    private $username;
-    // Password
-    private $password;
-    //data
-    private $act, $offset, $limit, $order, $filter;
+class ReferensiController extends Controller
+{
+    public function prodi()
+    {
+        $data = ProgramStudi::all();
 
+        return view('universitas.referensi.prodi', [
+            'data' => $data,
+        ]);
+    }
 
-    function __construct($act, $offset, $limit, $order, $filter) {
+    private function sync($act, $limit, $offset, $order, $filter)
+    {
+        $get = new FeederAPI($act, $offset, $limit, $order, $filter);
 
-        $this->url = env('FEEDER_URL');
-        $this->username = env('FEEDER_USERNAME');
-        $this->password = env('FEEDER_PASSWORD');
-        $this->act = $act;
-        $this->offset = $offset;
-        $this->limit = $limit;
-        $this->order = $order;
-        $this->filter = $filter;
+        $data = $get->runWS();
+
+        return $data;
+    }
+
+    public function sync_prodi()
+    {
+        $act = 'GetProdi';
+        $offset = 0;
+        $limit = 0;
+        $order = '';
+        $filter ='';
+
+        $prodi = new FeederAPI($act, $offset, $limit, $order, $filter);
+
+        $prodi = $prodi->runWS();
+
+        if (!empty($prodi['data'])) {
+            foreach ($prodi['data'] as $p) {
+                ProgramStudi::updateOrCreate(['id_prodi' => $p['id_prodi']], $p);
+            }
+        }
+
+        return redirect()->route('univ.referensi.prodi');
 
     }
 
-    public function runWS()
+    public function sync_all_pt()
     {
-        // dd($this->url);
-        $client = new Client(['timeout' => 120]);
-        $params = [
-            "act" => "GetToken",
-            "username" => $this->username,
-            "password" => $this->password,
-        ];
+        ini_set('max_execution_time', 0);
+        ini_set('memory_limit', '1G');
 
-        $req = $client->post( $this->url, [
-            'headers' => [
-                'Content-Type' => 'application/json',
-                'Accept' => 'application/json',
-            ],
-            'body' => json_encode($params)
-        ]);
+        $act = 'GetAllPT';
+        $offset = 0;
+        $limit = 500;
+        $order = 'id_perguruan_tinggi';
+        $filter ='';
+        $countAct = "GetCountPerguruanTinggi";
 
-        $response = $req->getBody();
-        $result = json_decode($response,true);
-        
-        if($result['error_code'] == 0) {
-            $token = $result['data']['token'];
-            $params = [
-                "token" => $token,
-                "act"   => $this->act,
-                "offset" => $this->offset,
-                "order" => $this->order,
-                "limit" => $this->limit,
-                "filter" => $this->filter,
-            ];
-            
-            $req = $client->post( $this->url, [
-                'headers' => [
-                    'Content-Type' => 'application/json',
-                    'Accept' => 'application/json',
-                ],
-                'body' => json_encode($params, JSON_UNESCAPED_SLASHES)
-            ]);
+        $count = $this->sync($countAct, $limit, $offset, "", "");
+        // dd($count);
+        for ($i = 0; $i < $count['data']; $i += $limit) {
+            $req = $this->sync($act, $limit, $i, $order, $filter);
 
-            $response = $req->getBody();
-
-            $result = json_decode($response,true);
-
+            if (isset($req['data']) && is_array($req['data'])) {
+                AllPt::upsert($req['data'], 'id_perguruan_tinggi');
+            }
         }
 
-        return $result;
+        return redirect()->route('univ.referensi.prodi')->with('success', 'Sinkronisasi Data Perguruan Tinggi Berhasil!');
+    }
+
+    public function sync_referensi()
+    {
+        ini_set('max_execution_time', 0);
+        ini_set('memory_limit', '1G');
+
+        $ref = [
+            ['act' => 'GetLevelWilayah', 'primary' => 'id_level_wilayah', 'model' => LevelWilayah::class],
+            ['act' => 'GetWilayah', 'primary' =>'id_wilayah', 'model' => Wilayah::class],
+            ['act' => 'GetNegara', 'primary' => 'id_negara', 'model' => Negara::class],
+            ['act' => 'GetStatusMahasiswa', 'primary' => 'id_status_mahasiswa', 'model' => \App\Models\StatusMahasiswa::class],
+            ['act' => 'GetSemester', 'primary' => 'id_semester', 'model' => \App\Models\Semester::class],
+            ['act' => 'GetJenisKeluar', 'primary' => 'id_jenis_keluar', 'model' => \App\Models\JenisKeluar::class],
+            ['act' => 'GetJenisPendaftaran', 'primary' => 'id_jenis_daftar', 'model' => \App\Models\JenisDaftar::class],
+            ['act' => 'GetJalurMasuk', 'primary' => 'id_jalur_masuk', 'model' => \App\Models\JalurMasuk::class],
+            ['act' => 'GetJenisEvaluasi', 'primary' => 'id_jenis_evaluasi', 'model' => \App\Models\JenisEvaluasi::class],
+            ['act' => 'GetIkatanKerjaSdm', 'primary' => 'id_ikatan_kerja', 'model' => \App\Models\IkatanKerja::class],
+            ['act' => 'GetJenisSubstansi', 'primary' => 'id_jenis_substansi', 'model' => \App\Models\JenisSubstansi::class],
+            ['act' => 'GetListSubstansiKuliah', 'primary' => 'id_substansi', 'model' => \App\Models\Perkuliahan\SubstansiKuliah::class],
+            ['act' => 'GetPembiayaan', 'primary' => 'id_pembiayaan', 'model' => \App\Models\Referensi\Pembiayaan::class],
+            ['act' => 'GetJenisAktivitasMahasiswa', 'primary' => 'id_jenis_aktivitas_mahasiswa', 'model' => \App\Models\Referensi\JenisAktivitasMahasiswa::class],
+            ['act' => 'GetKategoriKegiatan', 'primary' => 'id_kategori_kegiatan', 'model' => \App\Models\Referensi\KategoriKegiatan::class],
+            ['act' => 'GetAgama', 'primary' => 'id_agama', 'model' => \App\Models\Referensi\Agama::class],
+            ['act' => 'GetAlatTransportasi' , 'primary' => 'id_alat_transportasi', 'model' => \App\Models\Referensi\AlatTransportasi::class],
+            ['act' => 'GetPekerjaan' , 'primary' => 'id_pekerjaan', 'model' => \App\Models\Referensi\Pekerjaan::class],
+            ['act' => 'GetJenisPrestasi' , 'primary' => 'id_jenis_prestasi', 'model' => \App\Models\Referensi\JenisPrestasi::class],
+            ['act' => 'GetTingkatPrestasi' , 'primary' => 'id_tingkat_prestasi', 'model' => \App\Models\Referensi\TingkatPrestasi::class],
+            ['act' => 'GetProfilPT' , 'primary' => 'id_perguruan_tinggi', 'model' => \App\Models\ProfilPt::class],
+            // ['act' => 'GetAllPT', 'primary' => 'id_perguruan_tinggi', 'model' => \App\Models\Referensi\AllPt::class],
+        ];
+
+        foreach ($ref as $r) {
+            $act = $r['act'];
+            $offset = 0;
+            $limit = 0;
+            $order = '';
+            $filter ='';
+
+            $data = $this->sync($act, $limit, $offset, $order, $filter);
+
+            if (isset($data['data']) && !empty($data['data'])) {
+
+                if ($act == 'GetWilayah') {
+                    $data['data'] = array_map(function($d) {
+                        $d['id_wilayah'] = trim($d['id_wilayah']);
+                        $d['id_induk_wilayah'] = trim($d['id_induk_wilayah']);
+                        return $d;
+                    }, $data['data']);
+                }
+
+                if ($act == 'GetJenisSubstansi') {
+                    $data['data'] = array_map(function($d) {
+                        $d['id_jenis_substansi'] = trim($d['id_jenis_substansi']);
+                        return $d;
+                    }, $data['data']);
+                }
+
+                if($act == 'GetStatusMahasiswa')
+                {
+                    // add new status mahasiswa id_status_mahasiswa = 'K', nama_status_mahasiswa = 'Keluar'
+                    $newStatusMahasiswa = [
+                        ['id_status_mahasiswa' => 'K', 'nama_status_mahasiswa' => 'Keluar'],
+                        ['id_status_mahasiswa' => 'D', 'nama_status_mahasiswa' => 'Drop-Out/Putus Studi'],
+                        ['id_status_mahasiswa' => 'U', 'nama_status_mahasiswa' => 'Menunggu Ujian'],
+                        ['id_status_mahasiswa' => 'L', 'nama_status_mahasiswa' => 'Lulus'],
+                    ];
+                    array_push($data['data'], ...$newStatusMahasiswa);
+                }
+
+                if($act == 'GetAgama')
+                {
+                    $agama = [
+                        ['id_agama' => 98, 'nama_agama' => 'Tidak Diisi'],
+                    ];
+                    array_push($data['data'], ...$agama);
+                }
+
+                if($act == 'GetAlatTransportasi')
+                {
+                    $at = [
+                        ['id_alat_transportasi' => 2, 'nama_alat_transportasi' => 'Kendaraan Pribadi'],
+                    ];
+                    array_push($data['data'], ...$at);
+                }
+
+                $r['model']::upsert($data['data'], $r['primary']);
+            }
+        }
+
+        return redirect()->route('univ.referensi.prodi')->with('success', 'Sinkronisasi Data Referensi Berhasil!');
+
     }
 }
