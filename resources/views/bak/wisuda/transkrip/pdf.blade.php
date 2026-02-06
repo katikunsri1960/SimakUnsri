8906e2f5c9ef98dfaa1ab28f8b5d3c946d42b38b
ryan_fitra <ryanfitraperdana@gmail.com>
2026-02-06 16:19:34 +0700

    update sp-1 anak

--- resources/views/bak/wisuda/transkrip/pdf.blade.php
+++ resources/views/bak/wisuda/transkrip/pdf.blade.php
@@ -188,9 +188,12 @@
     if($rowStyle == 'mk-small'){
         $MAX_ROWS_LEFT = 15;
     } elseif($rowStyle == 'mk-medium'){
-        $MAX_ROWS_LEFT = 28;
+        $MAX_ROWS_LEFT = 28;$d->id_prodi == '132e62cc-dfdc-437d-9df3-e5317f80a6ff'
+    } elseif($d->id_prodi == '132e62cc-dfdc-437d-9df3-e5317f80a6ff' || // Sp-1 Ilmu Kesehatan Anak
+    ){ 
+        $MAX_ROWS_LEFT = 42;
     } elseif($d->id_prodi == 'e2f2ac47-8844-456b-b525-482db9da0abf' || // Sp-1 Ilmu Penyakit Kulit dan Kelamin
-    $d->id_prodi == '132e62cc-dfdc-437d-9df3-e5317f80a6ff'){ // Sp-1 Ilmu Kesehatan Anak
+    ){ 
         $MAX_ROWS_LEFT = 57;
     } elseif($rowStyle == 'mk-large'){
         $MAX_ROWS_LEFT = 37;
