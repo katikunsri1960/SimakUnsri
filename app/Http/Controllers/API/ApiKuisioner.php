33acb381e139e219e60732011a2f0907761dd69f
HendraWijaya15 <hendrawijaya150896@gmail.com>
2026-09-02 13:00:11 +0700

    Tambah API  dan route  kuisoner

---
+++ app/Http/Controllers/API/ApiKuisioner.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\KuisonerQuestion;
+use App\Models\KuisonerAnswer;
+class ApiKuisioner extends Controller
+{
+
+    public function kuisoner_questions()
+    {
+        $kuisoner_questions = KuisonerQuestion::select(
+            'id',
+            'question_indonesia',
+            'question_engish',
+            'created_at'
+        )->paginate(500);
+
+        return response()->json([
+            'kuisoner_questions' => $kuisoner_questions
+        ]);
+    }
+
+
+    public function kuisoner_answers()
+    {
+        $kuisoner_answers = KuisonerAnswer::select(
+            'id',
+            'kuisoner_question_id',
+            'id_kelas_kuliah',
+            'id_registrasi_mahasiswa',
+            'nilai',
+            'created_at'
+        )->paginate(500);
+
+        return response()->json([
+            'kuisoner_answers' => $kuisoner_answers
+        ]);
+    }
+}
